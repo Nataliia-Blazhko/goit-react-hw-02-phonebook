615d07b45f5e9aee6529602a6380eafb48c3be19
Nataliia-Blazhko <blazhko.natasha91@gmail.com>
2021-06-11 17:27:30 +0300

    добавляет начало решения 2 задачи

---
+++ src/components/contactform/ContactForm.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// uuidv4();
+
+export class ContactForm extends Component {
+  render() {
+    return (
+      <div className="container">
+        <p className="subtitle">Name</p>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+        <p className="subtitle">Number</p>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+        />
+        <button className="addContact">Add contact</button>
+      </div>
+    );
+  }
+}
